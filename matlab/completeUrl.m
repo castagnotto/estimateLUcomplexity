0beb14b70e37c62918598fd3926e6120f48b849d
Alessandro Castagnotto <a.castagnotto@tum.de>
2017-11-16 17:12:40 +0100

    created function to complete urls with all information needed from MATLAB to access the respective url information

---
+++ matlab/completeUrl.m
@@ -0,0 +1,26 @@
+function    newLink  = completeUrl(link,url)
+
+    % Take single parts
+    urlSplit    = strsplit(url,'/');
+    linkSplit   = strsplit(link,'/');
+    
+    % Find the first non-empty part of linkSplit
+    while isempty(linkSplit{1}), linkSplit(1) = []; end
+    idx = find(strcmp(linkSplit{1},urlSplit));
+    if isempty(idx) % no common path
+        if ~strcmp(linkSplit{1}(end),':')
+            idx = length(urlSplit);         %prepent the whole url if no http, https or ftp was speficied
+        else
+            %The link is already valid
+            newLink = link;
+            return
+        end 
+    end
+    
+    % create the full link
+    newLink = strjoin([urlSplit(1),strjoin([urlSplit(2:idx-1),linkSplit],'/')],'//');
+
+    
+    
+    
+    
