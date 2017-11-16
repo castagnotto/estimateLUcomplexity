89f666740ab941f9de70efc4cc7c7852d221bb16
Alessandro Castagnotto <a.castagnotto@tum.de>
2017-11-16 18:29:11 +0100

    importMatrices - WIP - MATLAB does not seem able to download from ftp, system commands are also failing...

--- matlab/importMatrices.m
+++ matlab/importMatrices.m
@@ -15,7 +15,26 @@ fid = fopen('data/MatrixMarket_listOfMatrices.txt','w');
 fprintf(fid,'%s\n',matrices{:});
 fclose(fid);
 
-%   Download the matrices onto .data/
+%%   Download the matrices onto .data/
+
+% MATLAB CANNOT DOWNLOAD THE FILE FROM THE FTP SERVER...!
+% Using system commands instead...
+fid = fopen('data/batchDonwloadFiles.bat','w');
+fprintf(fid,'bitsadmin /reset \npause\n');
+fprintf(fid,'bitsadmin /create myDownloadJob\npause\n');
+fprintf(1,'bitsadmin /create myDownloadJob\n');
+
 for iMat = 1:nMatrices
-    
+    url     = matrices{iMat};
+    idxName = strfind(url,'/'); idxName = idxName(end);
+    target  = sprintf('%sdata%s%s','C:\Users\castagnotto\Documents\estimateLUcomplexity\matlab\',filesep,url(idxName+1:end)); 
+    fprintf(fid,'bitsadmin /addfile myDownloadJob %s %s\npause\n',url,target);
+    fprintf(1,'bitsadmin /addfile myDownloadJob %s %s\n',url,target);
 end
+
+fprintf(fid,'bitsadmin /resume myDownloadJob\n');
+fprintf(fid,'bitsadmin /monitor\n');
+fprintf(fid,'bitsadmin /reset \npause\n');
+fclose(fid);
+
+%now run the batch file on your own..
